c197244c6c11c64177c58ac009d3dbcd73879e7a
Falk Doering <falk.doering@fadoe.de>
2015-11-15 14:26:54 +0100

    revert autoloader

--- Module.php
+++ Module.php
@@ -4,6 +4,17 @@ namespace PhlyContact;
 
 class Module
 {
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ),
+            ),
+        );
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
